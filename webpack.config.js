3366dcaf7c876fc1f209983bc5031f4a24521b22
Guilherme <gui.barros.js@gmail.com>
2022-10-03 20:06:43 -0300

    Separate babel options

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: isDevelopment ? 'development' : 'production',
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     filename: 'game.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
+  resolve: {
+    extensions: ['.js']
+  },
   module: {
     rules: [
       {
-        test: /\*.m?js$/,
+        test: /\*.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
         }
       }
     ]
   },
-  devtool: process.env.NODE_ENV === 'production' ? false : 'eval',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   devServer: {
     static: './dist',
     port: 3000,
